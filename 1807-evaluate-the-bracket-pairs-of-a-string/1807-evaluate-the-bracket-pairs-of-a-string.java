5b06c60b88244f1b65eb411e94b248b2ff99a028
explainedbychhavi <explainedbychhavi.business@gmail.com>
2026-09-26 07:36:54 +0530

    Time: 36 ms (64.25%), Space: 91.2 MB (58.82%) - LeetHub

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string/1807-evaluate-the-bracket-pairs-of-a-string.java
@@ -0,0 +1,46 @@
+class Solution {
+    public String evaluate(String s, List<List<String>> knowledge) {
+        
+        HashMap<String, String> map = new HashMap<>();
+        
+        // Store key-value pairs
+        for (List<String> pair : knowledge) {
+            map.put(pair.get(0), pair.get(1));
+        }
+        
+        StringBuilder result = new StringBuilder();
+        
+        int i = 0;
+        
+        while (i < s.length()) {
+            
+            if (s.charAt(i) == '(') {
+                
+                int j = i + 1;
+                
+                // Find closing bracket
+                while (s.charAt(j) != ')') {
+                    j++;
+                }
+                
+                // Extract key
+                String key = s.substring(i + 1, j);
+                
+                // Replace with value or '?'
+                if (map.containsKey(key)) {
+                    result.append(map.get(key));
+                } else {
+                    result.append('?');
+                }
+                
+                i = j + 1;
+                
+            } else {
+                result.append(s.charAt(i));
+                i++;
+            }
+        }
+        
+        return result.toString();
+    }
+}
